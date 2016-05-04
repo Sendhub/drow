bc24e39f185793cce8fc103cd487d46ee554c0ed
Max Smythe <smythe@sendhub.com>
2016-05-04 13:12:53 -0700

    add in needed publishing data and egg

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ setup(
                      'off of Django that allows for easy querying, access '
                      'and data integrity, without requiring a given data '
                      'type.',
+    url='https://github.com/Sendhub/drow',
     author='Max Smythe',
+    author_email='max.smythe+drow@gmail.com',
     packages=find_packages(),
     install_requires=['riak'],
     license='MIT',
